5c09a9cdeb17dbfefe82b27fb6c87a282a19cc50
Kulikov Alexander <shouter84@yandex.ru>
2024-06-28 23:24:36 +0300

    added solution for task 2

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
+// 1) Дан массив const arr = [1, 5, 7, 9] с помощью Math.min и spread оператора,
+// найти минимальное число в массиве, решение задание должно состоять из одной
+// строки кода.
 
 const arr = [1,5,7,9];
 console.log(Math.min(...arr));
 
 
-function createCounter() {
-	const obj = {increment, decrement};
-	function increment(counter) {
-		return counter = counter + 1;
+
+// 2) Напишите функцию createCounter, которая создает счетчик и возвращает объект
+// с двумя методами: increment и decrement. Метод increment должен увеличивать
+// значение счетчика на 1, а метод decrement должен уменьшать значение счетчика
+// на 1. Значение счетчика должно быть доступно только через методы объекта,
+// а не напрямую.
+
+function createCounter(initialValue) {
+	let counter = initialValue;
+	function increment(value) {
+		return counter += value;
 	};
-	function decrement(counter) {
-		return counter = conter - 1;
+	function decrement(value) {
+		return counter -= value;
 	};
-	return obj;
+	function self() {
+		return counter;
+	}
+	return { increment, decrement, self};
 }
 
-const incrementFn = createCounter();
+const incrementFn = createCounter(6);
 console.log(incrementFn.increment(5));
+console.log(incrementFn.increment(3));
+console.log(incrementFn.decrement(2));
+console.log(incrementFn.decrement(8));
+console.log(incrementFn.self());
 
-const rootElement = document.getElementById('root');
+// 3) Дополнительное задание, выполняем только если проходили работу с DOM.
+// Напишите рекурсивную функцию findElementByClass, которая принимает корневой
+// элемент дерева DOM и название класса в качестве аргументов и возвращает первый
+// найденный элемент с указанным классом в этом дереве.
 
-console.log(rootElement);
+// Пример
+// const rootElement = document.getElementById('root');
+// const targetElement = findElementByClass(rootElement, 'my-class');
+// console.log(targetElement);
+
+// const rootElement = document.getElementById('root');
+
+// console.log(rootElement);
 
 // const myEl = document.querySelector('.my-class');
 // console.log(myEl.children[0].className);
